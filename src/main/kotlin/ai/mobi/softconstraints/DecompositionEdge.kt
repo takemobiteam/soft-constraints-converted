bdae309607cd2376fd40b97fef100d48a2b60d54
jcgentry <jcgentry@mac.com>
2025-01-27 16:34:46 -0500

    Rename files to match actual class names

--- src/main/kotlin/ai/mobi/softconstraints/DecompositionEdge.kt
+++ src/main/kotlin/ai/mobi/softconstraints/DecompositionEdge.kt
@@ -2,30 +2,6 @@ package ai.mobi.softconstraints
 
 import ai.mobi.softconstraints.serde.SerializedEdge
 
-class DecompositionEdge(
-    dictDecompositionEdge: SerializedEdge,
-    private val decomp: Decomposition
-) {
-    val source: DecompositionVertex
-    val target: DecompositionVertex
-
-    init {
-        val sourceName = dictDecompositionEdge.source
-        source = decomp.getVertexByName(sourceName)
-            ?: throw IllegalStateException("Edge uses undefined vertex $sourceName in decomposition ${decomp.name}. Fix and reload.")
-
-        val targetName = dictDecompositionEdge.target
-        target = decomp.getVertexByName(targetName)
-            ?: throw IllegalStateException("Edge uses undefined vertex $targetName in decomposition ${decomp.name}. Fix and reload.")
-
-        // Update input vertices of the target
-        target.inputVertices.add(source)
-    }
-
-    override fun toString(): String {
-        return "$source -> $target"
-    }
-}
 
 fun noteEdge(dictDecompositionEdge: SerializedEdge, decomp: Decomposition) {
     val sourceName = dictDecompositionEdge.source
